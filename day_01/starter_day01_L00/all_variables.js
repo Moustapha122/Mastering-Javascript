738b61eb012c28c9f0179c04846473e1c4c9ce8e
Moustapha122 <moustapha442@gmail.com>
2021-02-10 14:37:19 +0100

    changes

--- day_01/starter_day01_L00/all_variables.js
+++ day_01/starter_day01_L00/all_variables.js
@@ -1,1 +1,94 @@
 // *****Exercices: Data Types --> Level00 [6, 7, 8, 9, 10]*****
+
+
+// Declaring different variable data types
+var x, y, z;   
+x = "moustapha";          
+y = false;          
+z = undefined;     
+var etudiant = {nom:"isam", lastName:"loucif", age:23, eyeColor:"black"};
+etudiant = null;
+
+//varaible can also be declaring in one lineseprated by comma
+var x, y, z;
+
+//variable 
+String,Boolean,undefined,null;
+
+//Declarer quatre variable sans affecter des valeurs
+var a,b,c,d;
+//declarer quatre variable avec des valeurs
+var e,f,g,h;
+e="tapha";
+f=5;
+g= undefined;
+h=null;
+//sur plusieurs lignes
+var prenom="ahamat";
+var nom="moustapha";
+var matrimonial="celibataire";
+var pays="centrafrique";
+var age=28;
+//sur une ligne
+var prenom="mahamat";var nom="moustapha";var matrimonial="celibataire";var pays="centrafrique";var age=28.
+//var pour deux ages
+var myAge=26;
+var yourAge=30;
+console.log("j'ai " +myAge + "\nvous avez " +yourAge);
+console.log(myAge);
+console.log(yourAge);
+//exercice Level01
+var defi = "Pratiquer les algorithmes en JavaScript";
+//02
+console.log(defi);
+// 03
+console.log(defi.length) ;
+//04
+console.log(defi.toUpperCase());
+//05
+console.log(defi.toLowerCase());
+//06
+console.log(defi.substring(10,40));
+//07
+var jours="10 jours en JavaScript";
+console.log(jours.substring(3,22));
+//08
+console.log(jours. includes("Script"));
+//09
+
+var ecole="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(ecole.split())
+//12
+console.log(jours.replace("10 jours en JavaScript" , "10 Jours en php" ));
+//13
+console.log(jours.charAt(15));
+// caractere "a"
+// j C'est au troisieme
+console.log(jours.charAt(3));
+//15 
+var jours="10 jours en JavaScript";
+console.log(jours.indexOf("a"));
+//16
+var jours="10 jours en JavaScript";
+console.log(jours.lastIndexOf("a"));
+//17
+var phrase="Vous ne pouvez pas terminer une phrase avec parceque parceque c'est une conjonction";
+console.log(phrase.lastIndexOf("parceque"));
+//18
+console.log(phrase.lastIndexOf("parceque")); 
+//19
+console.log(phrase.search("parceque"));
+//20
+alert(jours.trim());
+//21
+
+
+
+
+
+
+
+
+
+
+
